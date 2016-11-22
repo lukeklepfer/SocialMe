f091ca462612d2c17448d8c631d9d049d2e9d1d2
Luke <lukeklepfer@gmail.com>
2016-11-22 16:24:42 -0600

    Pods and Setup.
    Firebase installed

---
+++ SocialMe/SignInVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SocialMe
+//
+//  Created by Luke Klepfer on 11/22/16.
+//  Copyright © 2016 Luke. All rights reserved.
+//
+
+import UIKit
+
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
