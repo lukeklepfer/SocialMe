cdec8c36919e2460ecc8a0dc39fec7d744b0d26a
Luke <lukeklepfer@gmail.com>
2016-11-22 15:51:05 -0600

    Initial Commit

---
+++ SocialMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialMe
+//
+//  Created by Luke Klepfer on 11/22/16.
+//  Copyright © 2016 Luke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
