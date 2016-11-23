be79ab2a2ddbce8edf77ca202cf1d2828f5b56f6
Luke <lukeklepfer@gmail.com>
2016-11-23 16:45:21 -0600

    Facebook Login kinda
    IDK if it’s really working.

--- SocialMe/SignInVC.swift
+++ SocialMe/SignInVC.swift
@@ -7,13 +7,44 @@
 //
 
 import UIKit
-
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func facebookBtn(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Facebook Auth Login: \(error)")
+            }else if result?.isCancelled == true {
+                //no error but user didnt allow it to open
+                print("User cancelled Facebook Auth")
+            }else{
+                print("Facebook Auth Succeded")
+                let cred = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(cred)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Unable to authenticate with Firebase: \(error)")
+            }else{
+                print("Authenticated with Firebase")
+            }
+        })
     }
 
 
