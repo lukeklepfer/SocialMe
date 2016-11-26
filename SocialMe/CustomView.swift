ebe477f11a04f6a507fef3d524d2206745b732e7
Luke <lukeklepfer@gmail.com>
2016-11-25 21:25:21 -0600

    UI-Done

--- SocialMe/CustomView.swift
+++ SocialMe/CustomView.swift
@@ -17,6 +17,7 @@ class CustomView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5
         layer.shadowOffset = CGSize(width: 1, height: 1)
+        layer.cornerRadius = 2.0
     }
 
 }
