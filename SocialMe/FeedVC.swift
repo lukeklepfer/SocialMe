15df1fd363de773aeb6a1d1b1998611b01a13d9d
Luke <lukeklepfer@gmail.com>
2016-11-25 22:37:16 -0600

    Small Changes
    UI changes, Data model prep

--- SocialMe/FeedVC.swift
+++ SocialMe/FeedVC.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
     }
-
+    
+    @IBAction func postBtnTapped(_ sender: Any) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell")!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+    }
 
 }
